937cc3d3ed68dc244f1a564092e4b8be2ec1a2df
André Alves <a95033@alunos.uminho.pt>
2023-12-24 15:59:36 +0000

    Beggining of day 3

--- Advent-2023/Day3.py
+++ Advent-2023/Day3.py
@@ -1,14 +1,28 @@
 from icecream import ic
 
+def allSymbols(lines):
+    lista = []
+    for line in lines:
+        line2 = line.strip()
+        ic(line2)
+        for symbol in line2:
+            if not symbol.isdigit() and symbol != ".":
+                # append the location of the symbol
+                lista.append((lines.index(line), line.index(symbol)))
+    return lista
+
+def solve(lines):
+    lista = allSymbols(lines)
+    return lista
 
 
 def readFile():
-    file = open("Day2Input.txt", "r")
-    # file = open("test.txt", "r")
+    # file = open("Day3Input.txt", "r")
+    file = open("Advent-2023/test.txt", "r")
     lines = file.readlines()
     file.close()
     return lines
 
 if __name__ == "__main__":
     lines = readFile()
-    ic(gameIds2(lines))
+    ic(solve(lines))
