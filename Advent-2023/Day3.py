158adecf4f2730b38a51b60e4be5aaff0807728f
André Alves <a95033@alunos.uminho.pt>
2023-12-24 16:05:09 +0000

    Update Day3.py

--- Advent-2023/Day3.py
+++ Advent-2023/Day3.py
@@ -13,6 +13,27 @@ def allSymbols(lines):
 
 def solve(lines):
     lista = allSymbols(lines)
+
+    nums = []
+    # now search the if there are numbers adjacent to the symbols
+    for symbol in lista:
+        if symbol[0] != 0:
+            # check the symbol above, only if it is not the first line
+            if lines[symbol[0]-1][symbol[1]].isdigit():
+                ic(lines[symbol[0]-1][symbol[1]])
+        # check the symbol below, only if it is not the last line
+        if symbol[0] != len(lines)-1:
+            if lines[symbol[0]+1][symbol[1]].isdigit():
+                ic(lines[symbol[0]+1][symbol[1]])
+        # check the symbol to the left, only if it is not the first column
+        if symbol[1] != 0:
+            if lines[symbol[0]][symbol[1]-1].isdigit():
+                ic(lines[symbol[0]][symbol[1]-1])
+        # check the symbol to the right, only if it is not the last column
+        if symbol[1] != len(lines[symbol[0]])-1:
+            if lines[symbol[0]][symbol[1]+1].isdigit():
+                ic(lines[symbol[0]][symbol[1]+1])
+
     return lista
 
 
