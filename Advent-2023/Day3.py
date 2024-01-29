71fc46083641b0fff5f012e7614841e2d0504971
André Alves <nseiporamor@gmail.com>
2024-01-29 20:57:18 +0000

    Completed day 3

--- Advent-2023/Day3.py
+++ Advent-2023/Day3.py
@@ -46,6 +46,19 @@ def hasSymbolAdjacent(lines, col, row):
                 return True
     return False
 
+def hasGearAdjacent(lines, col, row):
+    # check if there is a symbol adjacent to the number, aka not a '.' or a number
+    rows, cols = len(lines), len(lines[0])
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+    
+    for dr, dc in directions:
+        new_row, new_col = row + dr, col + dc
+        if 0 <= new_row < rows and 0 <= new_col < cols:
+            if lines[new_row][new_col] in ["*"]:
+                ic(lines[new_row][new_col], new_row, new_col)
+                return True, (new_row, new_col)
+    return False, None
+
 '''
 Change way of thought
 
@@ -58,13 +71,53 @@ Change way of thought
 4.2. If it is False, continue
 '''
 
+def solve2(lines):
+    nums = []
+    gears = {}
+    for i, line in enumerate(lines):
+        num = ""
+        gear = None
+        done = False
+        checking = False
+        for j, char in enumerate(line):
+            if char.isdigit():
+                checking = True
+                num += char
+                # check if the char is adjacent to a symbol, make done True
+                tmp, gear2 = hasGearAdjacent(lines, j, i)
+                if tmp:
+                    done = True
+                    gear = gear2
+            else:
+                if checking:
+                    if done:
+                        ic(num)
+                        if gear not in gears:
+                            gears[gear] = []
+                        gears[gear].append(int(num))
+                        # nums.append(int(num))
+                    num = ""
+                    gear = None
+                    done = False
+                    checking = False
+    ic(gears)
+    for gear in gears:
+        if len(gears[gear]) > 1:
+            nums.append(gears[gear][0] * gears[gear][1])
+    return sum(nums)
+    
+    
+
 def readFile():
     file = open("Advent-2023/Day3Input.txt", "r")
-    # file = open("Advent-2023/test.txt", "r")
+    file2 = open("Advent-2023/test.txt", "r")
     lines = file.readlines()
+    lines2 = file2.readlines()
     file.close()
-    return lines
+    file2.close()
+    return lines, lines2
 
 if __name__ == "__main__":
-    lines = readFile()
-    ic(solve(lines))
+    lines, lines2 = readFile()
+    # ic(solve(lines))
+    ic(solve2(lines))
